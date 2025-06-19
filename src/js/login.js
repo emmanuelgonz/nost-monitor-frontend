5848aeae7ecd9f7c744b72cca88d8e1874136fd3
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2025-06-19 14:43:03 -0700

    use keycloak.token

--- src/js/login.js
+++ src/js/login.js
@@ -3,44 +3,17 @@ import $ from "jquery";
 import Keycloak from "keycloak-js";
 import { connect, updateAmqpToken, setUserExchange } from "./main";
 
-function fetchAccessToken() {
-  console.log(KEYCLOAK_HOST, KEYCLOAK_PORT, KEYCLOAK_REALM, KEYCLOAK_CLIENT_ID);
-  return fetch(`https://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams({
-      'client_id': KEYCLOAK_CLIENT_ID,
-      'client_secret': KEYCLOAK_CLIENT_SECRET,
-      'grant_type': 'client_credentials'
-    })
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then(text => {
-          console.error("Failed to obtain access token:", text);
-          throw new Error('Failed to obtain access token');
-        });
-      }
-    })
-    .then(data => {
-      return data.access_token;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+let keycloak; // Make keycloak accessible in all functions
 
 function startTokenRefresh() {
   setInterval(() => {
-    fetchAccessToken().then(newToken => {
-      if (newToken) {
-        updateAmqpToken(newToken); // Call update in main.js
+    keycloak.updateToken(60).then((refreshed) => {
+      if (refreshed) {
+        updateAmqpToken(keycloak.token);
         console.log("Access token refreshed.");
       }
+    }).catch(() => {
+      console.error("Failed to refresh token");
     });
   }, 3 * 60 * 1000); // Refresh every 3 minutes
 }
@@ -50,15 +23,13 @@ function startApplication() {
   $("#navLogout")
     .text("Logout " + keycloak.tokenParsed.preferred_username)
     .show();
-  
-  fetchAccessToken().then(token => {
-    if (token) {
-      connect(token);
-      startTokenRefresh();
-    } else {
-      console.error("Could not fetch AMQP access token.");
-    }
-  });
+
+  if (keycloak.token) {
+    connect(keycloak.token);
+    startTokenRefresh();
+  } else {
+    console.error("No Keycloak token available.");
+  }
 
   $("#navLogout").on("click", () => {
     keycloak.logout();
@@ -88,7 +59,6 @@ loginModal.show();
 // const KEYCLOAK_CLIENT_SECRET = process.env.DEFAULT_KEYCLOAK_CLIENT_SECRET;
 
 $("#loginForm").on("submit", (e) => {
-  loginModal.hide();
   e.preventDefault();
   const KEYCLOAK_HOST = $("#loginKeycloakHost").val();
   const KEYCLOAK_PORT =  parseInt($("#loginKeycloakPort").val());
@@ -98,7 +68,7 @@ $("#loginForm").on("submit", (e) => {
   const KEYCLOAK_CLIENT_SECRET = $("#loginKeycloakClientSecret").val() || process.env.DEFAULT_KEYCLOAK_CLIENT_SECRET;
   console.log(KEYCLOAK_CLIENT_ID);
 
-  const keycloak = new Keycloak({
+  keycloak = new Keycloak({
     url: `https://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}/`,
     realm: KEYCLOAK_REALM,
     clientId: KEYCLOAK_WEB_LOGIN_CLIENT_ID,
@@ -114,60 +84,7 @@ $("#loginForm").on("submit", (e) => {
         console.error("User not authenticated");
       }
     })
-    .catch(function () {
-      console.error("Failed to initialize Keycloak");
+    .catch(function (err) {
+      console.error("Failed to initialize Keycloak", err);
     });
 });
-
-// // Set Keycloak fields (do NOT pre-fill Client ID and Secret)
-// $("#loginKeycloakHost").val(process.env.DEFAULT_KEYCLOAK_HOST);
-// $("#loginKeycloakPort").val(process.env.DEFAULT_KEYCLOAK_PORT);
-// $("#loginKeycloakRealm").val(process.env.DEFAULT_KEYCLOAK_REALM);
-// $("#loginKeycloakWebLoginClientId").val(process.env.DEFAULT_KEYCLOAK_WEB_LOGIN_CLIENT_ID);
-// $("#loginExchange").val(process.env.DEFAULT_RABBITMQ_EXCHANGE);
-// const KEYCLOAK_CLIENT_ID = process.env.DEFAULT_KEYCLOAK_CLIENT_ID;
-// const KEYCLOAK_CLIENT_SECRET = process.env.DEFAULT_KEYCLOAK_CLIENT_SECRET;
-
-// const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
-// loginModal.show();
-
-// $("#loginForm").on("submit", function (e) {
-//   e.preventDefault();
-//   const KEYCLOAK_HOST = $("#loginKeycloakHost").val();
-//   // const KEYCLOAK_PORT = parseInt($("#loginKeycloakPort").val());
-//   const KEYCLOAK_REALM = $("#loginKeycloakRealm").val();
-//   const KEYCLOAK_WEB_LOGIN_CLIENT_ID = $("#loginKeycloakWebLoginClientId").val();
-//   const exchange = $("#loginExchange").val();
-
-//   if (exchange) {
-//     setUserExchange(exchange);
-//   }
-  
-//   console.log({
-//     KEYCLOAK_HOST,
-//     KEYCLOAK_PORT,
-//     KEYCLOAK_REALM,
-//     KEYCLOAK_WEB_LOGIN_CLIENT_ID,
-//     exchange,
-//   });
-
-//   const keycloak = new Keycloak({
-//     url: `https://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}/`,
-//     realm: KEYCLOAK_REALM,
-//     clientId: KEYCLOAK_WEB_LOGIN_CLIENT_ID,
-//   });
-
-//   keycloak
-//     .init({ onLoad: "login-required" })
-//     .then(function (authenticated) {
-//       if (authenticated) {
-//         startApplication();
-//       } else {
-//         console.error("User not authenticated");
-//       }
-//     })
-//     .catch(function () {
-//       console.error("Failed to initialize Keycloak");
-//     });
-
-// });
